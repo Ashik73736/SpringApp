43b9dad4e25523afcc71acfedea9ab8c53875a3e
Mohammed.AshikS <mohammed.ashiks@in.bosch.com>
2023-12-14 12:12:50 +0530

    The Second commit

---
+++ src/main/java/com/Nova_docker/Nova_docker/NovaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.Nova_docker.Nova_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Nova_docker/Nova_docker/NovaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nova_docker.Nova_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
